df44e2742de9770ce55986f62daa52624e7bdc70
Jooyoung Kim <yongin016@gmail.com>
2024-09-11 20:03:11 -0600

    add student-info file and complete the assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return (
+        <main>
+            <h1>Jooyoung Kim</h1>
+            <ul>
+                <li>
+                    <Link href="https://github.com/teddy-1991/cprg306-assignments.git">GitHub repository</Link>
+                </li>
+            </ul>
+        </main>
+    );
+}
