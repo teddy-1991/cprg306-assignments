294e994458cf72523010c6672f38ab88ed597fda
Jooyoung Kim <yongin016@gmail.com>
2024-09-19 19:21:50 -0600

    week-3 assignments completed

--- app/page.js
+++ app/page.js
@@ -3,12 +3,19 @@ import Link from "next/link";
 export default function Page() {
     return (
         <main>
-            <h1>CPRG 306: Web Development 2 - Assignments</h1>
-            <ul>
-                <li>
-                    <Link href="/week-2">Week 2 Assignment</Link>
-                </li>
-            </ul>
+            <div className = "flex flex-col justify-start items-center h-screen ">
+                <div className = "mt-32 w-3/4 bg-gray-100" >
+                    <div className = "text-center">
+                        <h1 className = "text-xl font-bold uppercase">CPRG 306: Web Development 2 - Assignments</h1>
+                    </div>
+                    <div className = "self-start">
+                        <ul>
+                            <li className = "hover:text-green-300"><Link href="/week-2">Week 2 Assignment</Link></li>
+                            <li className = "hover:text-green-300"><Link href="/week-3">Week 3 Assignment</Link></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </main>
     );
 }
---
+++ app/week-3/page.js
@@ -0,0 +1,10 @@
+import ItemList from './item-list';
+
+export default function Page() {
+    return (
+        <main>
+            <h1 className = "text-4xl font-bold text-center my-5">Shopping List</h1>
+            <ItemList />
+        </main>
+    )
+}
